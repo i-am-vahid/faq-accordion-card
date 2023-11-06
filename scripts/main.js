8dadef974e0a69a89c0cbccb6766bfcc9a6ba898
vahidm <mohammadVahid_mohammadi@proton.me>
2023-11-06 13:14:06 +0330

    fix something

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,37 @@
 let paragraphs = document.querySelectorAll('.question p')
 let pics = document.querySelectorAll('.question img')
 let boxs = document.querySelectorAll('.box')
-for (let par of paragraphs){
-    par.addEventListener('click',(e)=>{
-        let arrow = par.nextElementSibling
-        for (let par of paragraphs){
-            par.classList.remove('bold')
-        }
-        for (let pic of pics){
-            pic.classList.remove('rotate')
-        }
-        par.classList.toggle('bold')
-        arrow.classList.toggle('rotate')
-        for (let box of boxs ){
-            box.style.gridTemplateRows='1fr 0fr'
-        }
-        let box = e.target.parentElement.parentElement
-        box.style.gridTemplateRows='1fr 1fr'
-        box.classList.toggle('active')
+
+
+
+boxs.forEach(box=>{
+    box.addEventListener('click',()=>{
+        show(box)
     })
+})
+
+
+
+
+function show(box) {
+    let p = box.querySelector('.question p')
+    let pic = box.querySelector('.question img')
+    if (p.classList.contains('bold')){
+        p.classList.remove('bold')
+        pic.classList.remove('rotate')
+        box.style.gridTemplateRows='1fr 0fr'
+        console.log(1)
+    }
+    for (let par of paragraphs){
+        par.classList.remove('bold')
+    }
+    for (let pic of pics){
+        pic.classList.remove('rotate')
+    }
+    for (let box of boxs ){
+        box.style.gridTemplateRows='1fr 0fr'
+    }
+    p.classList.toggle('bold')
+    pic.classList.toggle('rotate')
+    box.style.gridTemplateRows='1fr 1fr'
 }
